4815a84579c8bec7c9cb8990ced4c15c1c2742b4
aok207 <aungookhant007@gmail.com>
2023-10-15 10:21:42 +0630

    Finished implementing wiki page function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
+
+markdowner = markdown2.Markdown()
 
 # home page
 def index(request):
@@ -10,4 +12,13 @@ def index(request):
 
 # entry page
 def wiki_page(request, page_name):
-    return render(request, "encyclopedia/wiki.html", {"page": page_name.upper()})
+    # Get the content of the page using get entry function
+    page_content = util.get_entry(page_name)
+    # If the content is None, this will return None
+    if page_content is None:
+        content = None
+    # Else convert it into HTML, and return that instead
+    else:
+        content = markdowner.convert(page_content)
+
+    return render(request, "encyclopedia/wiki.html", {"page": page_name.upper(), "content": content})
