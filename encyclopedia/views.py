b171557ed0899af0409a2e2dab905610936f020a
aok207 <aungookhant007@gmail.com>
2023-10-15 10:02:59 +0630

    Created wiki_page and the title of that wiki_page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 from . import util
 
-
+# home page
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+# entry page
+def wiki_page(request, page_name):
+    return render(request, "encyclopedia/wiki.html", {"page": page_name.upper()})
